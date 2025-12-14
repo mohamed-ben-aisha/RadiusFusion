e0c77d495019887ece2cbe42880cc44832a1d5c9
Mohamed Ben-Aisha <mohammedibrahem94@gmail.com>
2025-12-14 07:02:30 +0200

    add user management improvements: restructure schema with localization, add credits field with migration, enhance table actions and columns

--- app/Filament/Admin/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Admin/Resources/Users/Tables/UsersTable.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Admin\Resources\Users\Tables;
 
-use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
@@ -15,14 +14,16 @@ class UsersTable
         return $table
             ->columns([
                 TextColumn::make('name')
+                    ->label(__('Name'))
                     ->searchable(),
                 TextColumn::make('username')
+                    ->label(__('Username'))
                     ->searchable(),
                 TextColumn::make('email')
-                    ->label('Email address')
+                    ->label(__('Email address'))
                     ->searchable(),
-                TextColumn::make('email_verified_at')
-                    ->dateTime()
+                TextColumn::make('credits')
+                    ->label(__('Credits'))
                     ->sortable(),
                 TextColumn::make('created_at')
                     ->dateTime()
@@ -37,12 +38,15 @@ class UsersTable
                 //
             ])
             ->recordActions([
-                EditAction::make(),
+                EditAction::make()
+                    ->label(__('Edit User'))
+                    ->hiddenLabel()
+                    ->icon('heroicon-o-pencil')
+                    ->hiddenLabel()
+                    ->tooltip(__('Edit')),
             ])
             ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
+                DeleteBulkAction::make(),
             ]);
     }
 }
